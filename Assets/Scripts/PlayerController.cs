195c3271481ee851aa8ee0a0fbb9dd2c33e3aa5d
joesobo <soboleskijoe@gmail.com>
2020-02-20 17:06:24 -0800

    added player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField]
+    private Vector2 vel = Vector2.zero;
+    [SerializeField]
+    private Vector2 pos;
+    public float speed = 0.5f;
+
+    private void Start() {
+        pos = this.transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        processKeys();
+
+        setLocation();
+    }
+
+    private void processKeys(){
+        if(Input.GetKey(KeyCode.RightArrow)){
+            vel.x += speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            vel.x -= speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.DownArrow)){
+            vel.y += speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.UpArrow)){
+            vel.y -= speed * Time.deltaTime;
+        }
+    }
+
+    private void setLocation(){
+        if(vel.x > 0){
+            pos.x += vel.x / 10;
+            vel.x -= Time.deltaTime;
+        }
+        if(vel.x < 0){
+            pos.x += vel.x / 10;
+            vel.x += Time.deltaTime;
+        }
+        if(Mathf.Abs(vel.x) < 0.01f){
+            vel.x = 0;
+        }
+
+        if(vel.y > 0){
+            pos.y -= vel.y / 10;
+            vel.y -= Time.deltaTime;
+        }
+        if(vel.y < 0){
+            pos.y -= vel.y / 10;
+            vel.y += Time.deltaTime;
+        }
+        if(Mathf.Abs(vel.y) < 0.01f){
+            vel.y = 0;
+        }
+
+        this.transform.position = pos;
     }
 }
