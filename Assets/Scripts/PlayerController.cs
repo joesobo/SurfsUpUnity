650d0c4bc7f46e9933bd455775e989076a06529e
joesobo <soboleskijoe@gmail.com>
2020-02-20 17:50:11 -0800

    add obstacles and rigidbody for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,17 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private Vector2 vel = Vector2.zero;
-    [SerializeField]
-    private Vector2 pos;
     public float speed = 0.5f;
+    private Rigidbody2D rb;
 
     private void Start() {
-        pos = this.transform.position;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update() {
         processKeys();
 
-        setLocation();
+        slowVelocity();
     }
 
     private void processKeys(){
@@ -27,21 +26,23 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKey(KeyCode.LeftArrow)){
             vel.x -= speed * Time.deltaTime;
         }
-        if(Input.GetKey(KeyCode.DownArrow)){
+        if(Input.GetKey(KeyCode.UpArrow)){
             vel.y += speed * Time.deltaTime;
         }
-        if(Input.GetKey(KeyCode.UpArrow)){
+        if(Input.GetKey(KeyCode.DownArrow)){
             vel.y -= speed * Time.deltaTime;
         }
     }
 
-    private void setLocation(){
+    private void FixedUpdate() {
+        rb.MovePosition(rb.position + (vel / 10));
+    }
+
+    private void slowVelocity(){
         if(vel.x > 0){
-            pos.x += vel.x / 10;
             vel.x -= Time.deltaTime;
         }
         if(vel.x < 0){
-            pos.x += vel.x / 10;
             vel.x += Time.deltaTime;
         }
         if(Mathf.Abs(vel.x) < 0.01f){
@@ -49,17 +50,13 @@ public class PlayerController : MonoBehaviour
         }
 
         if(vel.y > 0){
-            pos.y -= vel.y / 10;
             vel.y -= Time.deltaTime;
         }
         if(vel.y < 0){
-            pos.y -= vel.y / 10;
             vel.y += Time.deltaTime;
         }
         if(Mathf.Abs(vel.y) < 0.01f){
             vel.y = 0;
         }
-
-        this.transform.position = pos;
     }
 }
